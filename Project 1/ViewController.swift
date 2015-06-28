25702f1ac4abb06506408122c8c11ae4a44bde05
Jun Yin <Johnny@Juns-MacBook-Air.local>
2015-06-28 14:00:21 -0400

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Jun Yin on 6/28/15.
+//  Copyright (c) 2015 Jun Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
